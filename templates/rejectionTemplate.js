15b9401726de2d0d7c352ce43f15c6931f67b070
Akshat Prasad <akshatpsd2005@gmail.com>
2026-06-29 13:09:36 +0530

    Update rejectionTemplate.js

--- templates/rejectionTemplate.js
+++ templates/rejectionTemplate.js
@@ -39,6 +39,18 @@ We regret to inform you that your visitor request could not be approved.
 
 </div>
 
+<p>You can send Request again ( prefer after few days ) by using <a href="https://visitor-management-system-ilux.onrender.com/Visitor/index.html"
+style="
+background:#F59E0B;
+color:#ffffff;
+padding:14px 28px;
+text-decoration:none;
+border-radius:8px;
+font-weight:bold;
+">
+ Visitor Request Portal
+</a>
+
 <p style="color:#64748B;">
 For further assistance, please contact the administrator.
 </p>
